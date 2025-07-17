e5e6e3e070c79463ffeced7cd2994965c0c946f8
aileenwa <aileen.wallenhorst@fra-uas.de>
2025-07-17 12:20:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.distance import geodesic
+from io import BytesIO
 
 st.title("Distanzrechner: Kundenadresse vs. Parkposition")
 
